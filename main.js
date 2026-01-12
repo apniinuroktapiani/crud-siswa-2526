52ab6999ebc1d9848c5d5314b7b23db7c773fe16
apniinuroktapiani <apninuroktapianioktaviani@gmail.com>
2026-01-12 10:14:38 +0700

    menambah fitur ubah data siswa

--- main.js
+++ main.js
@@ -61,7 +61,7 @@ export async function tampilkanDaftarSiswa() {
       const kolomAksi = document.createElement("td")
       
       //buat tombol edit
-      const tombolEdit = document.createElement("button")
+      const tombolEdit = document.createElement("a")
       tombolEdit.textContent = "Edit"
       tombolEdit.href = "edit.html?id=" + id
       tombolEdit.className = "button edit"
@@ -121,3 +121,23 @@ export async function hapusSiswa(id) {
   
 }
 
+//fungsi untuk mengambil data siswa berdasarkan id
+//agar data ditampilkan di form ubah
+export async function ambilDataSiswa(id) {
+    const docRef = doc(db,"siswa", id)
+    const docSnap = await getDoc(docRef)
+    
+    return await docSnap.data()
+}
+
+//fungsi untuk mengubah data siswa
+export async function ubahDataSiswa(id, nis, nama, kelas) {
+  await updateDoc(doc(db, "siswa", id), {
+      nis: nis, 
+      nama: nama, 
+      kelas: kelas
+  })
+  
+  //alihkan kehalaman daftar siswa
+  window.location.href ='daftar.html'
+}
